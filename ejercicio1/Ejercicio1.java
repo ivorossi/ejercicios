a5bafba7f125b87699e853f3519f1a564609731a
ivo <rossiivomartin@gmail.com>
2022-12-04 23:34:24 -0300

    aplicando correcciones

--- ejercicio1/Ejercicio1.java
+++ ejercicio1/Ejercicio1.java
@@ -1,131 +1,132 @@
 package ejercicio1;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
 
 public class Ejercicio1 {
+	public static void main(String[] args) {
 
-	private char colorMancha;
-	private int tamanoDeMacha;
-	private int cont = 1;
-	private boolean[][] matrizLogica;
-
-	private void restartContador() {
-		this.cont = 1;
-	}
-
-	public void setMatrizLogica(int m, int n) {
-		matrizLogica = new boolean[m][n];
-	}
-
-	public void porDerecha(int i, int j, char[][] matriz) {
-		if (matriz[0].length - 2 < j)
-			return;
-		if (matriz[i][j] == matriz[i][j + 1]) {
-			if (!matrizLogica[i][j + 1]) {
-				cont++;
-				matrizLogica[i][j + 1] = true;
-				this.porDerecha(i, j + 1, matriz);
-				this.porAbajo(i, j + 1, matriz);
-				this.porArriba(i, j + 1, matriz);
-			}
-		}
-	}
-
-	public void porAbajo(int i, int j, char[][] matriz) {
-		if (matriz.length - 2 < i)
-			return;
-		if (matriz[i][j] == matriz[i + 1][j]) {
-			if (!matrizLogica[i + 1][j]) {
-				cont++;
-				matrizLogica[i + 1][j] = true;
-				this.porDerecha(i + 1, j, matriz);
-				this.porIzquierda(i + 1, j, matriz);
-				this.porAbajo(i + 1, j, matriz);
-			}
-		}
-	}
-
-	public void porArriba(int i, int j, char[][] matriz) {
-		if (1 > i)
-			return;
-		if (matriz[i][j] == matriz[i - 1][j]) {
-			if (!matrizLogica[i - 1][j]) {
-				cont++;
-				matrizLogica[i - 1][j] = true;
-				this.porDerecha(i - 1, j, matriz);
-				this.porIzquierda(i - 1, j, matriz);
-				this.porArriba(i - 1, j, matriz);
-			}
-		}
-	}
-
-	public void porIzquierda(int i, int j, char[][] matriz) {
-		if (1 > j)
-			return;
-		if (matriz[i][j] == matriz[i][j - 1]) {
-			if (!matrizLogica[i][j - 1]) {
-				cont++;
-				matrizLogica[i][j - 1] = true;
-				this.porIzquierda(i, j - 1, matriz);
-				this.porAbajo(i, j - 1, matriz);
-				this.porArriba(i, j - 1, matriz);
-			}
-		}
-	}
-
-	public String bucarMancha(char[][] matriz) {
-		for (int y = 0; y < this.matrizLogica.length; y++) {
-			for (int x = 0; x < this.matrizLogica[y].length; x++) {
-				if (!this.matrizLogica[y][x]) {
-					this.matrizLogica[y][x] = true;
-					this.porDerecha(y, x, matriz);
-					this.porIzquierda(y, x, matriz);
-					this.porAbajo(y, x, matriz);
-					this.porArriba(y, x, matriz);
-					if (this.tamanoDeMacha < this.cont) {
-						this.tamanoDeMacha = cont;
-						this.colorMancha = matriz[y][x];
+		Stream<String> imput = null;
+		try {
+			imput = Files.lines(Paths.get(args[0]));
+			StringBuilder output = new StringBuilder();
+			ArrayList<char[]> M1 = new ArrayList<char[]>();
+			AtomicBoolean flag = new AtomicBoolean(false);
+			Finder finder1 = new Finder();
+			imput.forEach(line -> {
+				if (line.charAt(0) == 35) {
+					if (flag.getAndSet(true)) {
+						output.append(finder1.findPaintStain(M1)).append("\n");
 					}
-					this.restartContador();
+					output.append(line.substring(line.length() - 1))
+						.append(". ");
+					M1.clear();
+					finder1.clear();
+				} else {
+					M1.add(line.toCharArray());
+				}
+			});
+			System.out.println(output.append(finder1.findPaintStain(M1)));
+		} catch (IOException e) {
+			System.out.println("No se encontro la direccion del archivo");
+		} 
+	}
+}
+
+class Finder {
+	
+	private ArrayList<int[]> positionPointer = new ArrayList<int[]>();
+	private char paintStain;
+	private int paintStainSize;
+	private int count = 1;
+	private boolean[][] countedPosition;
+
+	private void restartCounter() {
+		this.positionPointer.clear();
+		this.count = 1;
+	}
+
+	public void clear() {
+		this.restartCounter();
+		this.countedPosition = null;
+		this.paintStainSize = 0;
+		this.paintStain = 0;
+	}
+
+	private void setPointerCounted(int m, int n) {
+		countedPosition = new boolean[m][n];
+	}
+
+	private void byRight(int i, int j, ArrayList<char[]> M1) {
+		if (M1.get(i).length - 2 < j)return;
+		if (M1.get(i)[j] != M1.get(i)[j + 1])return;
+		if (countedPosition[i][j + 1])return;
+		this.count++;
+		this.countedPosition[i][j + 1] = true;
+		this.positionPointer.add(new int[] { i, j + 1 });
+
+	}
+
+	private void byDown(int i, int j, ArrayList<char[]> M1) {
+		if (M1.size() - 2 < i)return;
+		if (M1.get(i)[j] != M1.get(i + 1)[j])return;
+		if (countedPosition[i + 1][j])return;
+		count++;
+		countedPosition[i + 1][j] = true;
+		this.positionPointer.add(new int[] { i + 1, j });
+	}
+
+	private void byUp(int i, int j, ArrayList<char[]> M1) {
+		if (1 > i)return;
+		if (M1.get(i)[j] != M1.get(i - 1)[j])return;
+		if (countedPosition[i - 1][j])return;
+		count++;
+		countedPosition[i - 1][j] = true;
+		this.positionPointer.add(new int[] { i - 1, j });
+	}
+
+	private void byLeft(int i, int j, ArrayList<char[]> M1) {
+		if (1 > j)return;
+		if (M1.get(i)[j] != M1.get(i)[j - 1])return;
+		if (countedPosition[i][j - 1])return;
+		count++;
+		countedPosition[i][j - 1] = true;
+		this.positionPointer.add(new int[] { i, j - 1 });
+	}
+
+	private void findByAllDireccion(int i, int j, ArrayList<char[]> M1) {
+		this.countedPosition[i][j] = true;
+		this.byRight(i, j, M1);
+		this.byLeft(i, j, M1);
+		this.byDown(i, j, M1);
+		this.byUp(i, j, M1);
+	}
+
+	public StringBuilder findPaintStain(ArrayList<char[]> M1) {
+		this.setPointerCounted(M1.size(), M1.get(0).length);
+		for (int i = 0; i < this.countedPosition.length; i++) {
+			for (int j = 0; j < this.countedPosition[i].length; j++) {
+				if (!this.countedPosition[i][j]) {
+					this.positionPointer.add(new int[] { i, j });
+					for (int k = 0; k < positionPointer.size(); k++) {
+						this.findByAllDireccion(this.positionPointer.get(k)[0], this.positionPointer.get(k)[1], M1);
+					}
+					if (this.paintStainSize < this.count) {
+						this.paintStainSize = count;
+						this.paintStain = M1.get(i)[j];
+					}
+					this.restartCounter();
 				}
 			}
 		}
-		return "(" + (char) 34 + this.colorMancha + (char) 34 + ", " + this.tamanoDeMacha + ")";
-	}
-
-	public static void main(String[] args) throws IOException {
-		long start = System.currentTimeMillis();
-		FileReader fr = new FileReader(new File("ejercicio1-imput.txt"));
-		BufferedReader br = new BufferedReader(fr);
-		ArrayList<char[]> matrizFake = new ArrayList<char[]>();
-		Ejercicio1 a1 = new Ejercicio1();
-		boolean banderaCase = false;
-		String caso = "";
-		String linea;
-		do {
-			linea = br.readLine();
-			if (linea == null || 35 == (linea.charAt(0))) {
-				if (banderaCase) {
-					char[][] matriz = new char[matrizFake.size()][matrizFake.get(0).length];
-					for (int i = 0; i < matrizFake.size(); i++) {
-						matriz[i] = matrizFake.get(i);
-					}
-					a1.setMatrizLogica(matrizFake.size(), matrizFake.get(0).length);
-					System.out.print(caso + ": ");
-					System.out.println(a1.bucarMancha(matriz));
-				}
-				caso = linea;
-				banderaCase = true;
-				a1 = new Ejercicio1();
-				matrizFake.clear();
-			} else {
-				matrizFake.add(linea.toCharArray());
-			}
-		} while (linea != null);
-		System.out.println("runTime: " + (System.currentTimeMillis() - start) + "ms");
+		return new StringBuilder("(")
+				.append(this.paintStain)
+				.append(", ")
+				.append(this.paintStainSize)
+				.append(")");
 	}
 }
