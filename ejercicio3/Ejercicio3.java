94c9c63faaf8b3f8015cf068779b3139cb1eae60
ivo <rossiivomartin@gmail.com>
2022-12-20 17:24:10 -0300

    inicial commit espero feedback

---
+++ ejercicio3/Ejercicio3.java
@@ -0,0 +1,37 @@
+package ejercicio3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) throws IOException {
+		try (Stream<String> inputLines = Files.lines(Paths.get("ejercicio3-input"))) {
+			AtomicInteger caseNumber = new AtomicInteger();
+			inputLines.forEach(line -> {
+				if (line.startsWith("#")) {
+					caseNumber.set(Integer.parseInt(line.replaceAll("\\D+", "")));
+				} else {
+					System.out.println(String.format("%s. %s", caseNumber, DigitalClock.howItLooksInTheMirror(line)));
+				}
+			});
+		} catch (IOException e) {
+			throw new IOException("no se pudo encontrar la direccion del archivo");
+		}
+	}
+}
+class DigitalClock{
+	static final String[]SAME_TIMES_IN_THEE_MIRROR = {"00:00", "02:50", "05:20", "20:05"};
+	static public String howItLooksInTheMirror(String time) {
+		for(String sameTime :SAME_TIMES_IN_THEE_MIRROR) {
+			if(time.equals(sameTime)){
+				return "Se ve igual";
+			}
+		}
+		return "No se ve igual";
+	}
+}
