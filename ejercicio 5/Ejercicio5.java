91cbcfc318715710311ceb1fd52aeb153725efd7
ivo <rossiivomartin@gmail.com>
2023-01-04 18:21:29 -0300

    solucion con serie de tribonacci

--- ejercicio 5/Ejercicio5.java
+++ ejercicio 5/Ejercicio5.java
@@ -27,19 +27,38 @@ public class Ejercicio5 {
 }
 
 class Window {
-	
+
 	static public int treeWindowsInRowWhithCourtain(int windows) {
 		/*
 		 * for all possible combinations of windows with or without curtains, returns
 		 * the number of combinations where there are three windows in a row with
-		 * curtains
+		 * curtains 0 0 1 3 8 20 47 107 238 520 1121
 		 */
-		int amountMatch = 0;
-		for (int i = 0; i < Math.pow(2, windows); i++) {
-			if (Integer.toBinaryString(i).contains("111")) {
-				amountMatch++;
+		return (int) Math.pow(2, windows) - Serie.getTribonacci(windows + 3);
+	}
+}
+
+class Serie {
+
+	static int getTribonacci(int element) {
+		int[] tribonaciSerie = { 0, 0, 1 };
+		if (element < 3) {
+			return tribonaciSerie[element];
+		} else {
+			int tribonaciElement = 0;
+			for (int i = 2; i < element; i++) {
+				tribonaciElement = tribonaciSerie[2] + tribonaciSerie[1] + tribonaciSerie[0];
+				/*
+				 * no se queda bien asi, o si hago las asignaciones con un bucle for, son la
+				 * misma canntidad de lineas supongo que no vale la pena, y tampoco se si vale
+				 * la pena esta clase pero como son cosas distintas supongo que esta bien
+				 * separarlos, despues borro el comentario.
+				 */
+				tribonaciSerie[0] = tribonaciSerie[1];
+				tribonaciSerie[1] = tribonaciSerie[2];
+				tribonaciSerie[2] = tribonaciElement;
 			}
+			return tribonaciElement;
 		}
-		return amountMatch;
 	}
 }
